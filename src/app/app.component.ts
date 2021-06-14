9f1d8d348d080509e8f79934ba1cb81222340732
Prathap <prathapr585@gmail.com>
2021-06-14 09:23:07 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'vinod';
+export class AppComponent implements OnInit {
+  myForm: FormGroup;
+  formData1 = [];
+  formData2 = [];
+  formData3 = [];
+
+  ngOnInit() {
+    this.myForm = new FormGroup({
+      firstName: new FormControl(''),
+      lastName: new FormControl(''),
+      middleName: new FormControl(''),
+      email: new FormControl(''),
+      phNum: new FormControl(''),
+      gender: new FormControl(''),
+      nickName: new FormControl(''),
+    });
+  }
+
+  onSubmit(form: FormGroup) {
+    this.formData1.push({
+      firstName: form.value.firstName,
+      lastName: form.value.lastName,
+      middleName: form.value.middleName,
+      nickName: form.value.nickName
+    })
+    this.formData2.push({
+      email: form.value.email,
+      phNum: form.value.phNum,
+    })
+    this.formData3.push({
+      gender: form.value.gender,
+    })
+  }
 }
